68de92bd87f4cb3afe9edb8fa1816fcd8155902f
e.djoumatchoua <e.djoumatchoua@groupeonepoint.com>
2025-06-30 14:34:31 +0200

    Filter order amount greater than 10

--- models/intermediate/int_orders_filter.sql
+++ models/intermediate/int_orders_filter.sql
@@ -2,7 +2,7 @@ with cleaned_orders as (
     select * from {{ ref('stg_raw__orders') }}
     where order_id is not null
     and order_customer_id is not null
-    and order_amount > 0 
+    and order_amount > 10
 )
 
 select * from cleaned_orders
